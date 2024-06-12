a444a11a7b7ab801ef733fcd48e38cb8586c787e
Aryan Patel <patelaryan1726@gmail.com>
2024-06-12 19:00:36 +0530

    test changes

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ const { analyzeCryptoData } = require("./openai");
 const delayTime = 10000;
 
 const getDataFromApi = async () => {
-  const symbol = "ETH";
-  const smaResp = await getSMA("BTC");
+  const symbol = "XRP";
+  const smaResp = await getSMA(symbol);
   await delay(delayTime);
-  const rsiResp = await getRSI("BTC");
+  const rsiResp = await getRSI(symbol);
   await delay(delayTime);
-  const demaResp = await getDEMA("BTC");
+  const demaResp = await getDEMA(symbol);
   await delay(delayTime);
   const getccquotes = await getCryptocurrencyQuotes([symbol]);
   const resp = getccquotes[symbol];
@@ -24,11 +24,12 @@ const getDataFromApi = async () => {
   const marketCap = resp[0].quote.USD.market_cap;
 
   console.log({ symbol, smaResp, rsiResp, demaResp, volumeIn24hr, marketCap });
+  console.log(smaResp);
 };
 
 const main = async () => {
   const analysedData = await analyzeCryptoData(data);
-  console.log(await analysedData.choices[0].message);
+  console.log(analysedData.choices[0].message);
 };
 
 main().catch((err) => {
