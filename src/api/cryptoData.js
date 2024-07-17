944d274e5dc2c5c5b544842aa58eb0ecee78711f
md-aryan-pate <aryan.patel@minddeft.net>
2024-07-17 11:26:35 +0530

    aggregated data

--- src/api/cryptoData.js
+++ src/api/cryptoData.js
@@ -85,15 +85,18 @@ const getCryptocurrencyQuotes = async (symbols) => {
     return genericResponse("error", error);
   }
 };
-
+let fearAndGreedIndex = null;
 const getFearAndGreedIndex = async () => {
-  try {
-    const response = await axios.get("https://api.alternative.me/fng/");
+  if (fearAndGreedIndex === null) {
+    try {
+      const response = await axios.get("https://api.alternative.me/fng/");
 
-    return response.data.data[0].value;
-  } catch (error) {
-    return genericResponse("error", error);
+      fearAndGreedIndex = response.data.data[0].value;
+    } catch (error) {
+      return genericResponse("error", error);
+    }
   }
+  return fearAndGreedIndex;
 };
 
 const getPriceAppirciationInPercentage = async (
@@ -148,6 +151,54 @@ const getHourlyData = async (symbol) => {
   await saveDaataToFile(data);
 };
 
+const fetchLatestNews = async (token) => {
+  token = token + " token, cryptocurrency";
+  try {
+    const response = await axios.get("https://newsdata.io/api/1/latest", {
+      params: {
+        apikey: process.env.NEW_API,
+        q: token,
+        language: "en",
+      },
+    });
+
+    return response.data.results;
+  } catch (error) {
+    // Handle any errors
+    console.error("Error fetching news:", error);
+    throw error;
+  }
+};
+
+async function getTokenByMarketCap(rank) {
+  const url =
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+  const params = {
+    start: rank,
+    limit: 1,
+    convert: "USD",
+  };
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        "X-CMC_PRO_API_KEY": process.env.CMC_KEY,
+      },
+      params: params,
+    });
+
+    if (response.data.data && response.data.data.length > 0) {
+      const sym = response.data.data[0].symbol;
+      return sym;
+    } else {
+      throw new Error("No data returned for the given rank");
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+}
+
 const getDataFromApi = async (symbol) => {
   const smaResp = await getSMA(symbol);
   await delay(delayTime);
@@ -156,27 +207,87 @@ const getDataFromApi = async (symbol) => {
   const demaResp = await getDEMA(symbol);
   await delay(delayTime);
   const getccquotes = await getCryptocurrencyQuotes([symbol]);
+  await delay(delayTime);
   const resp = getccquotes[symbol];
+  const quote = resp[0].quote.USD;
 
-  const volumeIn24hr = resp[0].quote.USD.volume_24h;
-  const marketCap = resp[0].quote.USD.market_cap;
+  console.log(" Got basic data");
+
+  const volumeIn24hr = quote.volume_24h;
+  const percentageChange1hr = quote.percent_change_1h;
+  const percentageChange24hr = quote.percent_change_24h;
+  const percentageChange7d = quote.percent_change_7d;
+  const marketCap = quote.market_cap;
+  const fdv = quote.fully_diluted_market_cap;
   const activeAddresses = 1234567890;
   const fearGreedIndex = await getFearAndGreedIndex();
+  await delay(delayTime);
+  console.log(" Got FGI");
   const priceAppriciation = await getPriceAppirciationInPercentage(symbol);
-
-  const data = {
+  await delay(delayTime);
+  console.log(" Got price appriciation");
+  /*
+  const result = await fetchLatestNews(symbol);
+  let news = "";
+  if (result.length > 0) {
+    news = result[0].description;
+  }
+ */
+  const respData = {
     symbol,
     sma: smaResp.value,
     rsi: rsiResp.value,
     dema: demaResp.value,
     volume: volumeIn24hr,
+    percentageChange1hr,
+    percentageChange24hr,
+    percentageChange7d,
     marketCap,
     activeAddresses,
     fearGreedIndex,
     priceAppriciation,
+    fdv,
   };
-  return data;
+  return respData;
 };
+
+const main = async () => {
+  let rank = 8748;
+  while (true) {
+    let resData;
+    try {
+      let symbol;
+      try {
+        await delay(delayTime);
+        symbol = await getTokenByMarketCap(rank);
+      } catch (err) {
+        console.log(err);
+        continue;
+      }
+      if (symbol === null) continue;
+      console.log(`Got Token symbol ${symbol}`);
+      resData = await getDataFromApi(symbol);
+      resData["rank"] = rank + 1;
+      console.log("Got data from symbol");
+    } catch (err) {
+      console.log(err);
+      rank++;
+      continue;
+    }
+    saveDaataToFile(resData);
+    rank++;
+  }
+};
+
+const temp = async () => {
+  const res = await getTokenByMarketCap(12);
+  console.log(res);
+};
+
+main().catch((err) => {
+  console.log(err);
+  process.exitCode = 1;
+});
 
 module.exports = {
   getHourlyData,
--- src/openai/index.js
+++ src/openai/index.js
@@ -1,5 +1,5 @@
 const { OpenAI } = require("openai");
-const { prompts, returnPromptWithData } = require("../helper");
+const { returnPromptWithData } = require("../helper");
 require("dotenv").config();
 
 const openai = new OpenAI({
@@ -7,10 +7,28 @@ const openai = new OpenAI({
 });
 
 const analyzeCryptoData = async (data) => {
-  const prompt = returnPromptWithData(data);
+  // const prompt = returnPromptWithData(data);
+
+  const prompt = `{
+  symbol: 'CAKE',
+  sma: 1.7843333333333335,
+  rsi: 53.914229430527236,
+  dema: 1.7974000549504991,
+  volume: 25657824.36195749,
+  percentageChange1hr: -0.62091726,
+  percentageChange24hr: 3.15090321,
+  percentageChange7d: -14.03884663,
+  marketCap: 484341203.0476364,
+  activeAddresses: 1234567890,
+  fearGreedIndex: '27',
+  priceAppriciation: { trend: 'up', percentage: 2.6225769669327272 },
+  news: '',
+  fdv: 812106274.26
+} The provided data is based on current/live market based on the data provided can you give me the   
+similar tokens name and values from current/live market, state the date of from which you have aggregated the similar tokens data`;
 
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo-16k",
+    model: "gpt-4o",
     messages: [{ role: "system", content: prompt }],
   });
 
